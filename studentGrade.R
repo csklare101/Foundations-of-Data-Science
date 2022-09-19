221364d11956d89b94baa0b18942ed591c9c7798
csklare101 <77071807+csklare101@users.noreply.github.com>
2022-09-19 18:01:59 -0500

    Add files via upload

---
+++ studentGrade.R
@@ -0,0 +1,15 @@
+student = 90;
+
+if(student >= 90){
+  letterGrade = 'A'
+} else if(student >= 80){
+  letterGrade = 'B'
+} else if(student >= 70){
+  letterGrade = 'C'
+} else if(student >= 60){
+  letterGrade = 'D'
+} else{
+  letterGrade = 'F'
+}
+
+paste("Student has an ", letterGrade, "!", " Score was: ", student, sep = )
